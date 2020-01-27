6bfbccd143140cf5bfbcec68bd7eb03ffb259634
Rayon Hunte <rayon.hunte@gmail.com>
2020-01-27 07:09:35 -0400

    updates

--- interview/linked.js
+++ interview/linked.js
@@ -14,78 +14,161 @@ let myLink = {
   }
 };
 
-class LinkedList {
-  constructor(value) {
-    this.head = {
-      value: value,
-      next: null
-    };
-    this.tail = this.head;
-    this.length = 1;
-  }
-  append(value) {
-    const newNode = {value, next: null}
-    this.tail.next = newNode
-    this.tail = newNode
-    this.length += 1;
-    return this;
-  }
-  prepend(value) {
-    const newNode = {value, next: null}
-    newNode.next = this.head
-    this.head = newNode
-    this.length +=1
-    return this
-  }
-  insert(index, value) {
-      const newNode = {value, next: null}
-      let stop = this.head
-      let count = 0
-      while (stop) {
-          if(count === index) { break}
-          stop = stop.next
-          count++ //? 
-      }
-      let pointHolder = stop.next 
-      stop.next = newNode
-      newNode.next = pointHolder
-      this.length += 1
-      return this;
-  }
+// class LinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null
+//     };
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+//   append(value) {
+//     const newNode = {value, next: null}
+//     this.tail.next = newNode
+//     this.tail = newNode
+//     this.length += 1;
+//     return this;
+//   }
+//   prepend(value) {
+//     const newNode = {value, next: null}
+//     newNode.next = this.head
+//     this.head = newNode
+//     this.length +=1
+//     return this
+//   }
+//   insert(index, value) {
+//       const newNode = {value, next: null}
+//       let stop = this.head
+//       let count = 0
+//       while (stop) {
+//           if(count === index) { break}
+//           stop = stop.next
+//           count++ //? 
+//       }
+//       let pointHolder = stop.next 
+//       stop.next = newNode
+//       newNode.next = pointHolder
+//       this.length += 1
+//       return this;
+//   }
 
-  remove(index) {
-        let count = 0;
-        let lead = this.head 
-        while (count !== index -1) {
-            lead = lead.next            
-            count++
-        }   
-        const unwanted = lead.next
-        lead.next = unwanted.next
-        this.length --
+//   remove(index) {
+//         let count = 0;
+//         let lead = this.head 
+//         while (count !== index -1) {
+//             lead = lead.next            
+//             count++
+//         }   
+//         const unwanted = lead.next
+//         lead.next = unwanted.next
+//         this.length --
     
-  }
+//   }
 
-  print() {
-    let stop = this.head
-    const array = []
-    while(stop){
-        //array.push({value:stop.value, obj: stop.next})
-        array.push(stop.value)
-        stop = stop.next
+//   print() {
+//     let stop = this.head
+//     const array = []
+//     while(stop){
+//         //array.push({value:stop.value, obj: stop.next})
+//         array.push(stop.value)
+//         stop = stop.next
+//     }
+//     return array
+//   }
+// }
+
+// const newLinkedList = new LinkedList(10);
+
+// newLinkedList.append(5);
+// newLinkedList.append(6);
+// newLinkedList.append(7)
+// newLinkedList.prepend(8)
+// newLinkedList.prepend(9)
+// newLinkedList.insert(1, 11)
+// newLinkedList.remove(2)
+// newLinkedList.print() //?
+// console.log(newLinkedList);
+
+
+class LinkedList {
+    constructor(value) {
+      this.head = {
+        value: value,
+        next: null,
+        back: null
+      };
+      this.tail = this.head;
+      this.length = 1;
     }
-    return array
+    // tail = newNod
+    // new node is now tail
+
+    append(value) {
+     const newNode = {value, next: null, back: this.tail.value}
+     this.tail.next = newNode
+     this.tail = newNode
+     this.length += 1;
+     return this;
+    }
+    prepend(value) {
+      const newNode = {value, next: null, back: this.head.value}
+      newNode.next = this.head
+      this.head = newNode
+      this.length +=1
+      return this
+    }
+    insert(index, value) {
+        const newNode = {value, next: null, back: null}
+        let stop = this.head
+        let count = 0
+        while (stop) {
+            if(count === index) { break}
+            stop = stop.next
+            count++ //? 
+        }
+        let pointHolder = stop.next 
+        newNode.back = stop.value
+        stop.next = newNode
+        newNode.next = pointHolder
+        this.length += 1
+        return this;
+    }
+  
+    remove(index) {
+          let count = 0;
+          let lead = this.head 
+          while (count !== index -1) {
+              lead = lead.next            
+              count++
+          }   
+          const unwanted = lead.next
+          lead.next = unwanted.next
+          this.length --
+      
+    }
+  
+    print() {
+      let stop = this.head
+      const array = []
+      while(stop){
+          array.push({value:stop.value, obj: stop.next})
+          //array.push(stop.value)
+          stop = stop.next
+      }
+      return array
+    }
   }
-}
-
-const newLinkedList = new LinkedList(10);
-
+  
+  const newLinkedList = new LinkedList(10);
+  
 newLinkedList.append(5);
 newLinkedList.append(6);
-newLinkedList.append(7)
+//newLinkedList.append(7)
 newLinkedList.prepend(8)
-newLinkedList.prepend(9)
+//newLinkedList.prepend(9)
 newLinkedList.insert(1, 11)
-newLinkedList.remove(2)
-newLinkedList.print() //?
-console.log(newLinkedList);
+//   newLinkedList.remove(2)
+   newLinkedList.print() //?
+   console.log(newLinkedList);
+  
--- interview/play.js
+++ interview/play.js
@@ -1,25 +1,49 @@
-const a1 = ['a','b','n']
-const a2 = ['f', 'y', 'b']
+// const a1 = ['a','b','n']
+// const a2 = ['f', 'y', 'b']
 
 
 
-const common = (a1, a2) => { 
-    if (a1.length < 1 || a2.length < 1) return false
-    const a1Obj = {}
-    let status = false
+// const common = (a1, a2) => { 
+//     if (a1.length < 1 || a2.length < 1) return false
+//     const a1Obj = {}
+//     let status = false
     
-    for(i in a1) {
-        a1Obj[a1[i]] = a1[i]
-    }
+//     for(i in a1) {
+//         a1Obj[a1[i]] = a1[i]
+//     }
    
-    a2.forEach(e => {
-       if(e in a1Obj) {
-        status = true
-       }
-   });
+//     a2.forEach(e => {
+//        if(e in a1Obj) {
+//         status = true
+//        }
+//    });
 
-   return status
+//    return status
+// }
+
+
+// common(a1, a2) //?
+
+const obj = {   
+        value: 5,
+        next: {
+            value: 4,
+            next: null,
+            back: null
+        }
 }
 
+const obj2 = {   
+    value: 5,
+    next: {
+        value: 4,
+        next: {
+            value: 3,
+            next:null
+        },
+        back: {
+            value: 5
+        }
+    }
+}
 
-common(a1, a2) //?
