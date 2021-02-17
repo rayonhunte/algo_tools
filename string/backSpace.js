e443c89cb5e584273ca303d3e0b36f53c34452d9
rayonhunte <rayon.hunte@gmail.com>
2021-02-17 14:22:01 -0400

    update

--- string/backSpace.js
+++ string/backSpace.js
@@ -21,7 +21,7 @@
 // }
 
 
-//backspaceCompare(array, arrayTwo) //?
+// backspaceCompare(array, arrayTwo) //?
 
 
 
@@ -37,20 +37,69 @@
 // array = "bxj##tw"
 // arrayTwo = "bxo#j##tw"
 
-array = '#j'
-arrayTwo = '#j'
+// array = '#j'
+// arrayTwo = '#j'
 
 
-"bxo#j##tw"
+// "bxo#j##tw"
 
-const backspaceCompare = (s1, s2) => {
-    const a1 = s1.split('') //?
-    const a2 = s2.split('') //?
-    let match = true
-    right = 0
-    for(i = a1.length -1; i >= 0; i--) {
-       
+// const backspaceCompare = (s1, s2) => {
+//     const a1 = s1.split('') //?
+//     const a2 = s2.split('') //?
+//     let match = true
+//     right = 0
+//     for(i = a1.length -1; i >= 0; i--) {
+
+//     }
+//     return match
+// }
+const string1 = "ab#z"
+const string2 = "az#z"
+
+var backspaceCompare = function (S, T) {
+    let p1 = S.length - 1, p2 = T.length - 1;
+
+    while (p1 >= 0 || p2 >= 0) {
+        if (S[p1] === "#" || T[p2] === "#") {
+            if (S[p1] === "#") {
+                let backCount = 2;
+
+                while (backCount > 0) {
+                    p1--;
+                    backCount--;
+
+                    if (S[p1] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+
+            if (T[p2] === "#") {
+                let backCount = 2;
+
+                while (backCount > 0) {
+                    p2--;
+                    backCount--;
+
+                    if (T[p2] === "#") {
+                        backCount += 2;
+                    }
+
+                }
+            }
+        } else {
+            if (S[p1] !== T[p2]) {
+                return false;
+            } else {
+                p1--;
+                p2--;
+            }
+        }
     }
-    return match
-}
-backspaceCompare(array, arrayTwo) //?
+
+    return true;
+};
+
+
+
+backspaceCompare(string1, string2) //?
