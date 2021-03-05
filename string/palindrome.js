70c7516cae8e0f0cfd704875a21b51944717059c
rayonhunte <rayon.hunte@gmail.com>
2021-03-05 15:24:21 -0400

    updates

--- string/palindrome.js
+++ string/palindrome.js
@@ -15,12 +15,14 @@ let s5 = "a"
 let s6 = "ab"
 
 const isPalindrome = (s) => {
+    if(s.length <=1) {return true}
+    let bs = s.replace(/[^a-z0-9]+/gi,"").toLowerCase()
     let p1 = 0 //?
-    let p2 = s.length -1 //?
-    let oe = s.length % 2 //?
+    let p2 = bs.length -1 //?
+    let oe = bs.length % 2 //?
     while(oe === 0? p1 < p2 : p1 !== p2) {
-        if( s[p1] !== s[p2]) {
-            return [s[p1],s[p2]]
+        if( bs[p1] !== bs[p2]) {
+            return false;
         } else {
             s[p1] //?
             s[p2] //?
@@ -34,24 +36,10 @@ const isPalindrome = (s) => {
 
 const isPal = (s) => {
     if(s.length <=2) {return true}
-    let bs = s.replace(/[^a-z0-9]+/gi,"").toLowerCase()
-    bs
-    let remove = isPalindrome(bs) //?
-    if(remove !== true) {
-         let nbs = bs.replace(remove[0], '')
-         let nbs2 = bs.replace(remove[1], '')
-         nbs
-         nbs2
-         if (!isPalindrome(nbs)){
-          if (!isPalindrome(nbs2)){return false}
-          } 
-    }
-
-    
-    return true
 }
 
 //isPalindrome(s7) //?
 
-isPal(s3) //?
+isPal(s4) //?
+
 
