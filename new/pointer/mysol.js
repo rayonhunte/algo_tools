d614f99b34623daa545d71c7f1a50d756da79766
Rayon Hunte <rayon.hunte@gmail.com>
2021-07-08 21:44:11 -0400

    updates

---
+++ new/pointer/mysol.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+// which are they two numbers in the array that sum to zero
+
+
+
+const sumZero = (arr) =>  {
+    //check if array length is more than 2
+    if(arr.length < 2) return []
+    // set two point 
+    let start = 0;
+    let end = arr.length -1
+
+    
+    while(start < end) {
+        // sum the value of the two pointers
+        let base = (arr[end] + arr[start])
+        //  check if the  value is zero
+        if(base === 0) return [arr[start], arr[end]]
+        // move pointer if based on the value of vase
+        base < 0 ? start = start +1 : end = end -1
+    }
+    // return empty loop if now sums  equal to zero
+    return []
+}
+
+
+const arr = [-4,-3,-2,-1,0,1,2,3,4,5]
+
+// sumZero(arr) //?
+
+
+const countUniqueValue = (arr2) => {
+    if(arr2.length === 1 ) return 1
+    let start = arr2[0]
+    let end = arr2[1]
+
+}
+
+const arr2 = [1,1,1,1,2]
